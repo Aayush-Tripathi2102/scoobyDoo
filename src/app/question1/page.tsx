c7ec00a8f1b3c9a0ae7a86952ff4a47ab0dc0d7a
Aayush-Tripathi2102 <aayushtripathi2102@gmail.com>
2025-02-09 01:13:48 +0530

    added fetch

--- src/app/question1/page.tsx
+++ src/app/question1/page.tsx
@@ -1,8 +1,33 @@
+'use client';
 import LetterAnimation from '@/components/LetterAnimation'
 import AnimatedText from '@/components/text'
-import React from 'react'
+import React, { useState } from 'react'
 
 const page = () => {
+  const questionId = 'q1'
+  const [input, setInput] = useState('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+  
+  const handleSubmit = (e: React.FormEvent) => {
+      e.preventDefault();
+      console.log(input)
+    };
+
+  // const handleSubmit = (e: React.FormEvent) => {
+  //   e.preventDefault();
+  //   const isCorrect = checkCorrect(questionId, input)
+
+  //   if(isCorrect)
+  //   {
+  //     const user = appwriteService.getCurrentUser();
+
+  //   }
+  // };
+  
+
   return (
     <div>
         <div className="flex gap-2 ml-24 mt-10  text-2xl">
@@ -17,8 +42,8 @@ const page = () => {
             Can you uncover the secret concealed within the image?</LetterAnimation>
         </div>
         <div >
-          <form action="" className='flex gap-2 items-center mt-20 mx-auto'>
-            <input type="text" className='ml-64 bg-opacity-15 bg-[#11f800] rounded-none border-4 border-[#11f800] text-3xl outline-none focus:ring-0 uppercase focus:border-[#11f800] px-4 py-2' />
+          <form action="" className='flex gap-2 items-center mt-20 mx-auto' onSubmit={handleSubmit}>
+            <input type="text" value={input} className='ml-64 bg-opacity-15 bg-[#11f800] rounded-none border-4 border-[#11f800] text-3xl outline-none focus:ring-0 uppercase focus:border-[#11f800] px-4 py-2' onChange={handleChange}/>
             <button className=''>
                 <AnimatedText text="Submit" className = 'bg-[#11f800] transition-all text-black text-xl px-5 py-5' preStyle='bg-[#11f800] transition-all text-black  text-xl px-5 py-5'/>
             </button>
